480fb5b77caa9857d2538a82c69b73d08cc70a92
Matthew Tole <matthewtole@gmail.com>
2024-05-13 23:29:59 +0000

    Get the ID

--- Website/netlify/functions/button.mjs
+++ Website/netlify/functions/button.mjs
@@ -1,3 +1,6 @@
 export default async (req, context) => {
-  return new Response(req.url);
+  // get id query param from the request url using UrlSearchParams
+  const url = new URL(req.url);
+  const id = url.searchParams.get("id");
+  return new Response("Hello from button.mjs! id: " + id);
 };
