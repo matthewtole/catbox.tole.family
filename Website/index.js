08482d1206111f187f7dbd6146fcde7960cbc54c
Matthew Tole <matthewtole@gmail.com>
2024-05-14 00:22:14 +0000

    Start making the website functional

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,12 @@
+const panelPoop = document.querySelector(".panel[data-id='poop']");
+const panelWater = document.querySelector(".panel[data-id='water']");
+const panelFood = document.querySelector(".panel[data-id='food']");
+
 async function fetchData() {
   const res = await axios.get("/.netlify/functions/data");
-  console.log(res.data);
+  panelPoop.querySelector("time").innerText = res.data.poop.lastPressed;
+  panelWater.querySelector("time").innerText = res.data.water.lastPressed;
+  panelFood.querySelector("time").innerText = res.data.food.lastPressed;
 }
 
 fetchData().catch((err) => console.error(err));
