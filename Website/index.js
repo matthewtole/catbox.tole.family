c09f4d3d3ede9b87b144d71d3b3bce46e44769c9
Matthew Tole <matthewtole@gmail.com>
2024-05-14 00:37:07 +0000

    Fix luxon usage

--- Website/index.js
+++ Website/index.js
@@ -4,13 +4,13 @@ const panelFood = document.querySelector(".panel[data-id='food']");
 
 async function fetchData() {
   const res = await axios.get("/.netlify/functions/data");
-  panelPoop.querySelector(".time").innerText = DateTime.fromMillis(
+  panelPoop.querySelector(".time").innerText = luxon.DateTime.fromMillis(
     res.data.poop.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
-  panelWater.querySelector(".time").innerText = DateTime.fromMillis(
+  panelWater.querySelector(".time").innerText = luxon.DateTime.fromMillis(
     res.data.water.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
-  panelFood.querySelector(".time").innerText = DateTime.fromMillis(
+  panelFood.querySelector(".time").innerText = luxon.DateTime.fromMillis(
     res.data.food.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
 }
