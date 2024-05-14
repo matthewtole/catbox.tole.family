0e28d67ba201239952210e4f9b1633cd74598ccd
Matthew Tole <matthewtole@gmail.com>
2024-05-14 00:38:42 +0000

    I'm bad at this

--- Website/index.js
+++ Website/index.js
@@ -1,16 +1,18 @@
+const { DateTime } = luxon;
+
 const panelPoop = document.querySelector(".panel[data-id='poop']");
 const panelWater = document.querySelector(".panel[data-id='water']");
 const panelFood = document.querySelector(".panel[data-id='food']");
 
 async function fetchData() {
   const res = await axios.get("/.netlify/functions/data");
-  panelPoop.querySelector(".time").innerText = luxon.DateTime.fromMillis(
+  panelPoop.querySelector(".time").innerText = DateTime.fromMillis(
     res.data.poop.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
-  panelWater.querySelector(".time").innerText = luxon.DateTime.fromMillis(
+  panelWater.querySelector(".time").innerText = DateTime.fromMillis(
     res.data.water.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
-  panelFood.querySelector(".time").innerText = luxon.DateTime.fromMillis(
+  panelFood.querySelector(".time").innerText = DateTime.fromMillis(
     res.data.food.lastPressed
   ).toLocaleString(DateTime.DATETIME_MED);
 }
