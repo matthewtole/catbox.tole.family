0cb4254bf52f9599669970477a717d98416c97f1
Matthew Tole <matthewtole@gmail.com>
2024-10-07 12:28:58 -0700

    Update index.js

--- Website/index.js
+++ Website/index.js
@@ -20,7 +20,7 @@ const panels = {
 function updatePanel(id, data) {
   const element = panels[id].element;
   element.querySelector(".time").innerText = DateTime.now().minus({seconds: data.lastPressed}).toRelative();
-  if (Date.now() + data.lastPressed >= panels[id].delay) {
+  if (data.lastPressed >= panels[id].delay) {
     element.setAttribute("data-mode", "on");
   } else {
     element.setAttribute("data-mode", "off");
